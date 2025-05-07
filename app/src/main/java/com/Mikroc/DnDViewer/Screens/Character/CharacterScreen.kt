2c1994e6c6450bd6eee4188a9ac622fc405806a1
miquel.ros <miquel.ros@accenture.com>
2025-05-07 13:12:54 +0200

    FEAT: aplicat barra vida/mana a inventari en cas de que no tinguis fitxa ni homebrew

--- app/src/main/java/com/Mikroc/DnDViewer/Screens/Character/CharacterScreen.kt
+++ app/src/main/java/com/Mikroc/DnDViewer/Screens/Character/CharacterScreen.kt
@@ -7,20 +7,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
@@ -29,13 +24,9 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import com.Mikroc.DnDViewer.Components.CustomHpManaBar
-import com.Mikroc.DnDViewer.Components.InputCounter
 import com.Mikroc.DnDViewer.Models.CharacterModel
-import com.Mikroc.DnDViewer.Theme.blueMana
-import com.Mikroc.DnDViewer.Theme.discordRed
 import com.Mikroc.DnDViewer.R
 import com.Mikroc.DnDViewer.ViewModels.MainViewModel
 
